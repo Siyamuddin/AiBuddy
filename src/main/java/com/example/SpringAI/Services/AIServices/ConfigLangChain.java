8fe06159a6a8923ec9d4b06e018ef9accfe24d46
uddinsiyam <siyamuddin177@gmail.com>
2024-11-02 20:29:10 +0900

    secrete is safe now

--- src/main/java/com/example/SpringAI/Services/AIServices/ConfigLangChain.java
+++ src/main/java/com/example/SpringAI/Services/AIServices/ConfigLangChain.java
@@ -2,12 +2,14 @@ package com.example.SpringAI.Services.AIServices;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.ollama.OllamaChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.internal.bytebuddy.utility.dispatcher.JavaDispatcher;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -18,12 +20,21 @@ import java.time.Duration;
 @Slf4j
 public class ConfigLangChain {
 
-         @Bean
-         public ChatLanguageModel chatClient(){
-                     .baseUrl(Base_URL)
-                     .modelName(MODEL_NAME)
-                     .build();
-             return  model;
-         }
+    @Value("${ai.model.name}")
+    private String MODEL_NAME;
+    @Value("${ai.base.url}")
+    private String Base_URL;
+    @Value("${ai.api.key}")
+    private String API_KEY;
+
+    @Bean
+    public ChatLanguageModel chatClient(){
+        ChatLanguageModel model= OpenAiChatModel.builder()
+                .baseUrl(Base_URL)
+                .apiKey(API_KEY)
+                .modelName(MODEL_NAME)
+                .build();
+        return  model;
+    }
 
 }
